4797813327dc7a891ec74c2822c9dd8b1ed50c25
Javier Cardoso <cardoso.javier@outlook.com>
2025-04-25 00:55:22 -0500

    fix: load user tasks instead of all tasks

--- src/api/columns/getAllStatusColumn.ts
+++ src/api/columns/getAllStatusColumn.ts
@@ -5,29 +5,36 @@ export async function getAllStatusColumns(): Promise<StatusColumnProps[]> {
     try {
 
         const supabase = createClient();
-        
+        const userId = (await supabase.auth.getSession()).data.session?.user.id
+
+        if (!userId) {
+            console.error("Empty user id");
+            return [];
+        }
+
         const { data: statusColumns, error } = await supabase
             .from("statusColumn")
             .select(`
                 *,
                 task (
-                id,
-                description,
-                dueDate:due_date,
-                dueTime:due_time,
-                priority,
-                status,
-                createdAt:created_at,
-                updatedAt:updated_at,
-                columnId:column_id,
-                userAccount:users (
                     id,
-                    email,
-                    firstName:first_name,
-                    lastName:last_name
+                    description,
+                    dueDate:due_date,
+                    dueTime:due_time,
+                    priority,
+                    status,
+                    createdAt:created_at,
+                    updatedAt:updated_at,
+                    columnId:column_id,
+                    userAccount:users (
+                        id,
+                        email,
+                        firstName:first_name,
+                        lastName:last_name
+                    )
                 )
-                )
-            `);
+            `)
+            .eq("task.user_id", userId);
 
 
 
