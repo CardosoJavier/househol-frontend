3b0278d3fecfb7dd1a1dbcbcf4fe9a7f8095b1cc
Javier Cardoso <cardoso.javier@outlook.com>
2025-05-04 01:14:08 -0500

    fix background on disable

--- src/components/input/CustomInput.tsx
+++ src/components/input/CustomInput.tsx
@@ -17,10 +17,10 @@ export default function CustomInput({
 }) {
   return (
     <input
-      className={`h-10 w-full bg-transparent outline outline-secondary 
+      className={`h-10 w-full outline outline-secondary 
                 rounded-md px-2 py-3 text-accent text-sm focus-visible:outline-none 
                 duration-200 ease-linear focus-visible:ring-2 focus-within:ring-accent
-                ${isDisabled ? "bg-gray-200" : ""}
+                ${isDisabled ? "bg-gray-100" : "bg-transparent"}
                 ${type === "file" ? "hidden" : ""}
                 `}
       placeholder={placeholder}
