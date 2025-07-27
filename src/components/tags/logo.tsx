23b691373c35f562b3b5c387dbb76021ebf65401
Javier Cardoso <cardoso.javier@outlook.com>
2025-07-27 17:17:09 -0500

    broo yaa

--- src/components/tags/logo.tsx
+++ src/components/tags/logo.tsx
@@ -1,12 +1,12 @@
-export default function Logo({ size }: { size: number }) {
+export default function Logo({ size = 120 }: { size: number }) {
   return (
     <div className="flex items-center justify-center flex-shrink-0 w-36 h-12 max-h-12 max-w-36">
       <img
         src="/logo.png"
         alt="NeverShip Logo"
-        width={size ?? 120}
-        height={size ?? 120}
-        className="object-contain max-h-12 max-w-36"
+        width={size}
+        height={size}
+        className={`object-contain max-w-[${size}px] max-h-[${size}px]`}
         loading="eager"
       />
     </div>
